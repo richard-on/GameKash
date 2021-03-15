9090b847b9684d135100c5fce2ec21d91fe8cfe1
Ilya Gordey <wdybelievein@gmail.com>
2021-03-15 10:12:22 +0300

    added spizhenoe

---
+++ RoleGame/Character.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame
+{
+    enum Conditions { Normal, Weakened, Sick, Poisoned, Paralyzed, Dead }
+    enum Races { Human, Gnome, Elf, Orc, Goblin }
+    enum Genders { Male, Female }
+    class Character : IComparable
+    {
+        private static int Next_ID { get; set; } = 0;
+        public int ID { get; }
+        public string Name { get; }
+        public Conditions Condition { get; set; } = Conditions.Normal;
+        public bool AbilityToTalk { get; set; } = true;
+        public bool AbilityToMove { get; set; } = true;
+        public Races Race { get; }
+        public Genders Gender { get; }
+        private int age;
+        public int Age
+        { 
+            get
+            { 
+                return age;
+            } 
+            set
+            {
+                if (value < age)
+                    throw new Exception("Invalid Age value");
+                age = value;
+            }
+        }
+        private double current_health;
+        public double CurrentHealth
+        {
+            get
+            {
+                return current_health;
+            }
+            set
+            {
+                if (value > MaxHealth || value < 0)
+                    throw new Exception("Invalid Health value");
+                current_health = value;
+            }
+        }
+        public double MaxHealth { get; }
+        private int experience = 0;
+        public int Experience
+        {
+            get
+            {
+                return experience;
+            }
+            set
+            {
+                if (value < experience)
+                    throw new Exception("Invalid Age value");
+                experience = value;
+            }
+        }
+
+        public Character(string name, Races race, Genders gender, int age, double max_health)
+        {
+            if (name == null || name.Contains('\0'))
+                throw new Exception("Invalid Name value");
+            if(age < 0)
+                throw new Exception("Invalid Age value");
+            if(max_health <= 0)
+                throw new Exception("Invalid Health value");
+            ID = ++Next_ID;
+            Name = name;
+            Race = race;
+            Gender = gender;
+            Age = age;
+            MaxHealth = max_health;
+            CurrentHealth = MaxHealth;
+        }
+        public int CompareTo(object o)
+        {
+            Character character = o as Character;
+            if (character == null)
+                throw new Exception("Error");
+            return this.Experience.CompareTo(character.Experience);
+        }
+        public void Status()
+        {
+            if (CurrentHealth / MaxHealth < 0.1 && Condition == Conditions.Normal)
+                Condition = Conditions.Weakened;
+            else if (CurrentHealth / MaxHealth >= 0.1 && Condition == Conditions.Weakened)
+                Condition = Conditions.Normal;
+            if (CurrentHealth == 0)
+                Condition = Conditions.Dead;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"ID: {ID}\n" +
+                   $"Condition: {Condition}\n" +
+                   $"Race: {Race}\n" +
+                   $"Gender: {Gender}\n" + 
+                   $"Age: {Age}\n" +
+                   $"Max Health: {MaxHealth}\n" +
+                   $"Current Health: {CurrentHealth}\n" +
+                   $"Experience: {Experience}";
+        }
+    }
+}
---
+++ RoleGame/Wizard.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame
+{
+    class Wizard : Character
+    {
+        public double MaxMana { get; }
+        private double current_mana;
+        public double CurrentMana
+        {
+            get
+            {
+                return current_mana;
+            }
+            set
+            {
+                if (value > MaxMana || value < 0)
+                    throw new Exception("Invalid Mana value");
+                current_mana = value;
+            }
+        }
+        public Wizard(string name, Races race, Genders gender, int age, double max_health, double max_mana) 
+            :base(name, race, gender, age, max_health)
+        {
+            if (max_mana <= 0)
+                throw new Exception("Invalid Mana value");
+            MaxMana = max_mana;
+            CurrentMana = max_mana;
+        }
+        public override string ToString()
+        {
+            return base.ToString() +
+                   $"\nMax Mana: {MaxMana}\n" +
+                   $"Current Mana: {CurrentMana}";
+        }
+        public void AddHealth(Character character)
+        {
+            if (CurrentMana / 2 > character.MaxHealth - character.CurrentHealth)
+            {
+                CurrentMana -= (character.MaxHealth - character.CurrentHealth) * 2;
+                character.CurrentHealth = character.MaxHealth;
+            }
+            else
+            {
+                character.CurrentHealth += CurrentMana / 2;
+                CurrentMana = 0;
+            }
+        }
+    }
+}
