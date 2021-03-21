c394c1cc974567c9e801cf19c854b7441025d410
Ilya Gordey <wdybelievein@gmail.com>
2021-03-21 13:14:46 +0300

    add spizhenoe to main directory

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace GameKash
+namespace RoleGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Wizard Artas = new Wizard("Artas", Races.Human, Genders.Male, 30, 900, 400);
+            Wizard Kenarius = new Wizard("Kenarius", Races.Elf, Genders.Male, 100000, 4000, 1500);
+            Character Thunder = new Character("Thunder", Races.Orc, Genders.Male, 40, 2700);
+            Artas.CurrentHealth = 850;
+            Artas.Status();
+            //Console.WriteLine(Artas + "\n");
+            Kenarius.AddHealth(Artas);
+            Console.WriteLine(Kenarius + "\n");
+            Artas.Status();
+            Console.WriteLine(Artas);
         }
     }
 }
