c7ba698e8e592a12a78f6b4370e81e7f1f92ac8f
Ilya Gordey <wdybelievein@gmail.com>
2021-03-21 14:58:55 +0300

    add directories

---
+++ Spells/AddHealth.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameKash.Spells {
+    class AddHealth : Spell {
+
+    }
+}
---
+++ Spells/Spell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameKash {
+    abstract class Spell : IMagic {
+        private double MinMana;
+
+    }
+}
